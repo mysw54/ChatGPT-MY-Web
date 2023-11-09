f67bc13711b6ccb09dec13fe48af86bf71de2dfc
mysw54 <cn.tienon@gmail.com>
2023-11-09 21:44:42 +0800

    check err

--- app/api/common.ts
+++ app/api/common.ts
@@ -59,7 +59,7 @@ export async function requestOpenai(req: NextRequest) {
       }),
     },
     method: req.method,
-    body: clonedBody,
+    body: req.body,
     // to fix #2485: https://stackoverflow.com/questions/55920957/cloudflare-worker-typeerror-one-time-use-body
     redirect: "manual",
     // @ts-ignore
